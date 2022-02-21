54b405cbeb13cbb466c391107c3b3e11f9f2cbb4
Noopur Phalak <noopur@pixeldust.in>
2022-02-21 18:57:11 +0530

    Fixes

--- src/sampleComposable/index.ts
+++ src/sampleComposable/index.ts
@@ -1,1 +1,15 @@
-export * from './sampleComposable'
+import { ref } from 'vue'
+
+export const sampleComposable = () => {
+  const count = ref(0)
+
+  const increment = () => {
+    count.value += 1
+    console.log(count.value)
+  }
+
+  return {
+    count,
+    increment,
+  }
+}
